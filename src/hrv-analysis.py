899829dbf698566e770d09fca5569d5e49e57247
Milo Bauman <mjb01161@uga.edu>
2024-06-26 15:27:44 -0400

    Added example NeuroKit2 code

--- src/hrv-analysis.py
+++ src/hrv-analysis.py
@@ -1,1 +1,16 @@
-print("Hello)")
+import neurokit2 as nk
+import pandas as pd
+import numpy as np
+
+# Download example data
+data = pd.read_csv("https://raw.githubusercontent.com/neuropsychology/NeuroKit/master/data/bio_eventrelated_100hz.csv")
+
+# Preprocess the data (filter, find peaks, etc.)
+processed_data, info = nk.bio_process(ecg=data["ECG"], rsp=data["RSP"], eda=data["EDA"], sampling_rate=100)
+
+# Compute relevant features
+results = nk.bio_analyze(processed_data, sampling_rate=100)
+
+print(data)
+print(processed_data)
+print(results)
