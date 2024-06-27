b7331ccc445c7473d9743c4438d59d82f3f512e8
Milo Bauman <mjb01161@uga.edu>
2024-06-27 16:57:45 -0400

    Got program to accept example PPG data or BJS2 PPG data

--- src/hrv-analysis.py
+++ src/hrv-analysis.py
@@ -2,15 +2,28 @@ import neurokit2 as nk
 import pandas as pd
 import numpy as np
 
-# Download example data
-data = pd.read_csv("https://raw.githubusercontent.com/neuropsychology/NeuroKit/master/data/bio_eventrelated_100hz.csv")
+# Simulate PPG data
+ppg = nk.ppg_simulate(duration=40, sampling_rate=500, heart_rate=75, random_state=42)
 
-# Preprocess the data (filter, find peaks, etc.)
-processed_data, info = nk.bio_process(ecg=data["ECG"], rsp=data["RSP"], eda=data["EDA"], sampling_rate=100)
+# print(ppg)
 
-# Compute relevant features
-results = nk.bio_analyze(processed_data, sampling_rate=100)
+ppg_signals, info = nk.ppg_process(ppg, sampling_rate=500)
 
-print(data)
-print(processed_data)
-print(results)
+# ppg_real = nk.ppg_process()
+
+# Read the PPG data from CSV
+ppg = pd.read_csv("ppg_sensor_data.csv", header=None)
+
+
+ppg = nk.ppg_simulate(duration=400, sampling_rate=100, heart_rate=75, random_state=42)
+ppg = pd.read_csv("ppg_sensor_data.csv", header=None)
+print(ppg)
+
+ppg_cleaned = nk.ppg_clean(ppg, sampling_rate=100)
+print(ppg_cleaned)
+
+ppg_df, info = nk.ppg_process(ppg_cleaned, sampling_rate=100)
+print(ppg_df)
+print(info)
+
+print(nk.hrv(info['PPG_Peaks'], sampling_rate=100))
