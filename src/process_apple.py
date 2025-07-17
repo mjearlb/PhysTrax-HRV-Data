3bf7c6f5a97f3e8c7167f49befea1cb30d6c2332
Milo Bauman <mjb01161@uga.edu>
2025-07-17 12:57:51 -0400

    Added helper processing files.

---
+++ src/process_apple.py
@@ -0,0 +1,5 @@
+# This file takes an input of Apple Watch heart rate data 
+# in .csv format. 
+# 
+# It outputs a .csv in a standardized format to allow 
+# correlation with PhysTrax data to be calculated. 
---
+++ src/process_bangle.py
@@ -0,0 +1,5 @@
+# This file takes an input of PhysTrack heart rate data 
+# in .csv format. 
+# 
+# It outputs a .csv in a standardized format to allow 
+# correlation with Apple Watch data to be calculated. 
