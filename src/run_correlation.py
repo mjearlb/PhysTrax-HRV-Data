7397c98ca1ea530902749d9f42afd32293427237
Milo Bauman <mjb01161@uga.edu>
2025-07-17 12:59:49 -0400

    Added driver program

---
+++ src/run_correlation.py
@@ -0,0 +1,6 @@
+# This is the main program. Run this in order to find
+# the correlation between Apple Watch and PhysTrax
+# heart rate data. 
+# 
+# It takes an input of 2 .csv files. Format it like so: 
+# python3 run_correlation.py -a [apple_data].csv -p [phystrax_data].csv -o [output-name]
